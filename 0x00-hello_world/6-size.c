9f352769c3c6189def05939a2cee8cad3ff44310
kingsdomain <kingsdomain50@gmail.com>
2023-08-11 12:40:52 -0700

    Proper Grammer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main - a C program that prints the size of various
+* main - A C program that prints the size of various
 * types on the computer it is compiled and run on
-* Discription: The program discription
+* Discription: Print size of different data types
 * Return 0 (Success)
 */
 int main(void)
