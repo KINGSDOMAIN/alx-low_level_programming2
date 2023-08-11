649f7a6cd0ff95c1db33bc60a8cc9c4fd6801f4a
kingsdomain <kingsdomain50@gmail.com>
2023-08-11 14:52:05 -0700

    Hot fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ printf("size of a char: %lu byte(s)\n", (unsigned long)
 sizeof(a));
 printf("size of an int: %lu byte(s)\n", (unsigned long)
 sizeof(b));
-printf("size of a long: %lu bytes(s)\n", (unsigned long)
+printf("size of a long: %lu byte(s)\n", (unsigned long)
 sizeof(c));
-printf("size of a long long int: %lu bytes(s)\n", (unsigned long)
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)
 sizeof(d));
-printf("size of a float: %lu bytes(s)\n", (unsigned long)
+printf("size of a float: %lu byte(s)\n", (unsigned long)
 sizeof(f));
 return (0);
 }
